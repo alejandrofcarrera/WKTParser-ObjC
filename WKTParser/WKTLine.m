8bcf4977202bd18fef8fe95de5e79be75e5a36ba
Alejandro Fdez Carrera <alej4fc@gmail.com>
2014-08-04 12:06:59 +0200

    Line Class

---
+++ WKTParser/WKTLine.m
@@ -0,0 +1,53 @@
+//
+//  WKTLine.m
+//
+//  WKTParser Line
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 Alejandro Fdez Carrera
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#include "WKTLine.h"
+
+@implementation WKTLine
+
+- (id)init
+{
+    if(self == nil)
+    {
+        self = [super init];
+        self.type = @"Line";
+    }
+    return self;
+}
+
+- (id)initWithPoints:(NSArray *)points
+{
+    if (self == nil)
+    {
+        self = [self init];
+    }
+    [self removeListPoints];
+    [self setListPoints:points];
+    return self;
+}
+
+@end
