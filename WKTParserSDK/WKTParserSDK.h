3a7fccaa4a8506d5bc5c88b25e966f2a293b03a6
sivakspt <sivakspt@gmail.com>
2017-12-18 21:21:08 +0530

    Added framework so it can be used for carthage.

---
+++ WKTParserSDK/WKTParserSDK.h
@@ -0,0 +1,19 @@
+//
+//  WKTParserSDK.h
+//  WKTParserSDK
+//
+//  Created by Siva Kristipati on 12/18/17.
+//  Copyright © 2017 Alejandro F. Carrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WKTParserSDK.
+FOUNDATION_EXPORT double WKTParserSDKVersionNumber;
+
+//! Project version string for WKTParserSDK.
+FOUNDATION_EXPORT const unsigned char WKTParserSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WKTParserSDK/PublicHeader.h>
+
+
